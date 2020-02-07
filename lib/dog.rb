353163505e8613090b9943a5e296a784ad574305
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-07 06:13:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,10 @@
 class Dog 
-  
-  def name
+   def name
     @name 
   end 
   
   def name=(new_name)
     @name = new_name 
-  end 
-  
-  def initialize(breed)
-    @breed = breed
   end 
   
   def breed 
